c1d2c735409fd886632c3fa722c5cd14614395c4
cjhead <chead@linuxmail.org>
2021-08-31 21:54:19 -0400

    Initial commit

---
+++ sun_translator.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<string.h>
+
+
+typedef struct SunSymbol {
+    char word[30];
+    char symbol[30];
+} SunSymbol;
+
+
+int main()
+{
+    const char filename[] = "sun_dictionary.csv";
+    FILE *fh;
+
+    fh = fopen(filename,"r");
+    if(!fh)
+    {
+        printf("Unable to read from file %s\n",filename);
+        return(1);
+    }
+
+    /* Count total number of symbols */
+    int total_symbols = 0;
+    int ch;
+    while(!feof(fh))
+    {
+        ch = fgetc(fh);
+        if(ch == '\n')
+        {
+            total_symbols++;
+        }
+    }
+    rewind(fh);
+
+    /* Read in english/symbol translations */
+    SunSymbol symbols[total_symbols];
+    int i = 0;
+
+    char buffer[200];
+    fgets(buffer, 200, fh);
+
+    while(!feof(fh))
+    {
+        SunSymbol *s = symbols + i;
+
+        char *token = strtok(buffer, ",");
+        if(token)
+        {
+
+            strcpy(s->word, token);
+
+            char *token = strtok(NULL, ",");
+            strcpy(s->symbol, token);
+
+        }
+
+        fgets(buffer, 200, fh);
+        i++;
+
+    }
+
+    fclose(fh);
+
+    /* for(i = 0; i < total_symbols; i++)
+        printf("%s = %s", symbols[i].word, symbols[i].symbol); */
+
+    return(0);
+}
